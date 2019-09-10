ca9bdcc58e538a6b61af64e5308a3c9ae96ed0b5
Diego Garcia <diego.garcia@gstv.com>
2019-09-10 14:38:49 -0600

    Lint config, setup and fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,25 @@
-import React, { FunctionComponent, MouseEvent } from "react";
-import { connect } from "react-redux";
-import { useQuery } from "@apollo/react-hooks";
-import { RootState, selectors, actions } from "../../store";
-import { LEAGUES_QUERY, LeaguesQueryData } from "../../graphql/queries/Leagues";
-import { League } from "../../graphql/generated/types";
-import { withRouter, RouteComponentProps } from "react-router";
+import React, { FunctionComponent, MouseEvent } from 'react';
+import { connect } from 'react-redux';
+import { useQuery } from '@apollo/react-hooks';
+import { RootState, selectors, actions } from '../../store';
+import { LEAGUES_QUERY, LeaguesQueryData } from '../../graphql/queries/Leagues';
+import { League } from '../../graphql/generated/types';
+import { withRouter, RouteComponentProps } from 'react-router';
 
 // UI components
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import IconButton from "@material-ui/core/IconButton";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import IconButton from '@material-ui/core/IconButton';
 
 // Icons
-import MenuIcon from "@material-ui/icons/Menu";
+import MenuIcon from '@material-ui/icons/Menu';
 
 const mapStateToProps = (state: RootState) => ({
   localize: (key: string) =>
@@ -59,7 +59,7 @@ const Home: FunctionComponent<Props> = ({ history, localize, toggleMenu }) => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6">{localize("pages.home.title")}</Typography>
+          <Typography variant="h6">{localize('pages.home.title')}</Typography>
         </Toolbar>
       </AppBar>
       <List>
