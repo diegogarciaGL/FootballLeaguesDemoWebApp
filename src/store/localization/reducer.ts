a9cf909a5f5a48a8a0106b598db46c3ac8288bc9
Diego Garcia <diego.garcia@gstv.com>
2019-10-05 08:36:02 -0600

    Lint fix

--- src/store/localization/reducer.ts
+++ src/store/localization/reducer.ts
@@ -2,9 +2,7 @@ import * as localization from './actions';
 import { ActionType, getType } from 'typesafe-actions';
 import { LocalizationState } from './types';
 
-const {
-  REACT_APP_DEFAULT_LANGUAGE_ID: defaultLanguageId
-} = process.env;
+const { REACT_APP_DEFAULT_LANGUAGE_ID: defaultLanguageId } = process.env;
 
 export type LocalizationAction = ActionType<typeof localization>;
 
@@ -12,26 +10,29 @@ const defaultState: LocalizationState = {
   languageId: defaultLanguageId || 'es-CR',
   language: undefined,
   languages: []
-}
+};
 
-export default (state = defaultState, action: LocalizationAction): LocalizationState => {
+export default (
+  state = defaultState,
+  action: LocalizationAction
+): LocalizationState => {
   switch (action.type) {
     case getType(localization.fetchLanguage.success):
       return {
         ...state,
         language: action.payload
-      }
+      };
     case getType(localization.updateLanguage):
       return {
         ...state,
         languageId: action.payload
-      }
+      };
     case getType(localization.updateLanguages):
       return {
         ...state,
         languages: action.payload
-      }
+      };
     default:
       return state;
   }
-}
+};
